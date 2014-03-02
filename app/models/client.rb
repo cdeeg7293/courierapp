007472573a0df43e40ace4f19e9517a337575942
Olefav <olefav92@gmail.com>
2014-03-02 16:19:16 +0200

    Added address deletion code to courier and client models

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,20 +1,25 @@
 class Client < ActiveRecord::Base
-  has_many :contacts
+  after_initialize :create_associated, :if => :new_record?
+  after_destroy :destroy_address
+
+  has_many :contacts, dependent: :destroy
   accepts_nested_attributes_for :contacts, :reject_if => :all_blank, allow_destroy: true
 
   belongs_to :address
-  accepts_nested_attributes_for :address, :reject_if => :all_blank, update_only: true
+  accepts_nested_attributes_for :address,  :reject_if => :all_blank, update_only: true
 
   def full_name
     last_name + ' ' + first_name + ' ' + patronymic
   end
-
-  after_initialize :create_associated, :if => :new_record?
 
   private 
 
   def create_associated
     self.contacts.build if self.contacts.empty?
     self.build_address unless self.address
+  end
+
+  def destroy_address
+    self.destroy_address if self.address
   end
 end
--- app/models/courier.rb
+++ app/models/courier.rb
@@ -1,16 +1,21 @@
 class Courier < ActiveRecord::Base
+  after_initialize :create_associated_client, :if => :new_record?
+  after_destroy :destroy_address_living
+  
   belongs_to :client
-  accepts_nested_attributes_for :client, :reject_if => :all_blank, update_only: true
+  accepts_nested_attributes_for :client,  :reject_if => :all_blank, update_only: true
 
   belongs_to :address
   accepts_nested_attributes_for :address, :reject_if => :all_blank, update_only: true
-
-  after_initialize :create_associated_client, :if => :new_record?
 
 private
 
   def create_associated_client
     self.build_client unless self.client
     self.build_address unless self.address
+  end
+  
+  def destroy_address_living
+    self.destroy_address if self.address
   end
 end
