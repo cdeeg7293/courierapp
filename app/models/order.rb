aa2494bdad17a548d5bc9b0b103b10535979d3a0
Olefav <olefav92@gmail.com>
2014-03-10 08:11:43 +0200

    Some changes in order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,13 +2,18 @@ class Order < ActiveRecord::Base
   belongs_to :sender,   :class_name => Client
   belongs_to :receiver, :class_name => Client
   
-  before_create :set_issue_date
+  before_create :set_dates
   after_create :set_number
+  
+  def is_delivered?
+    return self.delivered_date < DateTime.current
+  end
   
   private
   
-  def set_issue_date
+  def set_dates
     self.issued_date = DateTime.current
+    self.delivered_date = Date.new(2099, 01, 01)
   end
   
   def set_number
