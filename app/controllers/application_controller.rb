2a8ca44a25197d297b113c1721a500fdb3d91938
Olefav <olefav92@gmail.com>
2014-03-10 10:06:07 +0200

    Started layout modification. Added navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  helper_method :model_list
+  
   #function for getting values from session
   def get_session_value(id)
     return session[id]
@@ -11,5 +13,10 @@ class ApplicationController < ActionController::Base
   #function for setting values to session
   def set_session_value(id, value)
     session[id] = value
+  end
+  
+  #list of models
+  def model_list
+    return model_list = ['clients', 'couriers', 'orders', 'contact_types']
   end
 end
