4ccac3a055b16cf9668039112eec1a72f1f23d52
Olefav <olefav92@gmail.com>
2014-03-15 14:04:45 +0200

    Added partial for all index pages, implemented that in clients index view, now orders index includes sender and receiver objects

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.all
+    @orders = Order.all.includes(:sender, :receiver)
   end
 
   # GET /orders/1
