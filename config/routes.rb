cbde7523118b7c8af65a247154029132e8243a21
Olefav <olefav92@gmail.com>
2014-04-27 15:05:51 +0300

    Started navbar auth part l10n

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Clientcontacttest::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'orders#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
