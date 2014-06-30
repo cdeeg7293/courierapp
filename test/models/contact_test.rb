24afeb263dc36ebad9f8fdde615131856fe6390d
Olefav <olefav92@gmail.com>
2014-06-30 17:18:19 +0300

    Added working test for contact model

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :contact_types
+  
+  test "should not save empty contact object" do
+    contact = Contact.new
+    assert_not contact.save, "Should not save a contact without type and value"
+    contact.contact_type = contact_types(:valid_contact_type)
+    assert_not contact.save, "Should not save a contact without value"
+    contact.value = "123455"
+    assert contact.save, "Should save a contact with type and value"
+  end
 end
