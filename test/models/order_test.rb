3f112c1474afe1fec006a3c7bb5b2479a7268675
Olefav <olefav92@gmail.com>
2014-06-30 19:50:44 +0300

    Created a test for order model, changed appropriate fixtures

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :orders
+  
+  test "validate sum" do
+    order = orders(:order_1)
+    order.sum = -1
+    assert_not order.valid?, "Negative sum passes validation"
+    order.sum = 0
+    assert_not order.valid?, "Zero sum passes validation"
+    order.sum = 1
+    assert order.valid?, "Positive sum does not pass validation"
+  end
+  
+  test "should not save empty order" do
+    order = Order.new
+    assert_not order.save
+  end
 end
