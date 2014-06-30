16088691156fcea67254184f14ab563757813965
Olefav <olefav92@gmail.com>
2014-06-30 17:21:12 +0300

    Added working test for contact_type model

--- test/models/contact_type_test.rb
+++ test/models/contact_type_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ContactTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validation test" do
+    contact_type = ContactType.new
+    assert_not contact_type.save, "Should not save a contact type without name"
+    contact_type.type_name = "Type name"
+    assert contact_type.save, "Should save a contact type with name"
+  end
 end
