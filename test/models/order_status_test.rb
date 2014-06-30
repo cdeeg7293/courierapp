64ba52cf0f9d2154d803b5258a4ef220a1b23e4b
Olefav <olefav92@gmail.com>
2014-06-30 19:58:56 +0300

    Added tests for order status model

--- test/models/order_status_test.rb
+++ test/models/order_status_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class OrderStatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :order_statuses
+  
+  test "should save a valid order status" do
+    order_status = order_statuses(:order_status_1)
+    assert order_status.save
+  end
+  
+  test "should not save an emty order status" do 
+    order_status = OrderStatus.new
+    assert_not order_status.save
+  end
 end
