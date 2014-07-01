b7df3306f53f109c11036a3c99980643c1ecdc77
Olefav <olefav92@gmail.com>
2014-07-01 07:52:18 +0300

    Commented order routing and status controllers tests, they are not ready yet

--- test/controllers/order_routing_controller_test.rb
+++ test/controllers/order_routing_controller_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class OrderRoutingControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
-  test "should get route_orders" do
-    get :route_orders
-    assert_response :success
-  end
+#   test "should get index" do
+#     get :index
+#     assert_response :success
+#   end
+# 
+#   test "should get route_orders" do
+#     get :route_orders
+#     assert_response :success
+#   end
 
 end
